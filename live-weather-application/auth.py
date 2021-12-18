794302283fd45b26a2f50a6aaee04a36b311ae47
Rohan Gosavi <64428362+rohangosavi01@users.noreply.github.com>
2021-12-18 00:52:11 -0600

    Update auth.py

--- live-weather-application/auth.py
+++ live-weather-application/auth.py
@@ -2,8 +2,7 @@ import requests, json
 
 def request_weather(city):
     pass
-    # Authentication Details have been removed from here, you can follow instructions from OpenWeather API to write this here
+    # Authentication Details have been removed from here
 
-    return data
 
 
