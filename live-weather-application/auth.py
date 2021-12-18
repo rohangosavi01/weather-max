8ae10aae4ede638248033203fa1fbb2e4d428dc7
Rohan Gosavi <64428362+rohangosavi01@users.noreply.github.com>
2021-12-18 00:51:43 -0600

    Update auth.py

--- live-weather-application/auth.py
+++ live-weather-application/auth.py
@@ -1,13 +1,8 @@
 import requests, json
 
 def request_weather(city):
-    token = '18adfd08d3ac94dfa82cff135fef7927'
-    base_url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+token
-    response = requests.get(base_url)
-    data = response.json()
-    
-    if data['cod'] == '404' or data['cod'] == 404:
-        return 'city-not-found'
+    pass
+    # Authentication Details have been removed from here, you can follow instructions from OpenWeather API to write this here
 
     return data
 
